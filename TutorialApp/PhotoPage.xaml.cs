91dd83af5894bec6178be3ebbea4fede5f75ff55
Justin W <Nope@nada.com>
2012-10-17 21:08:20 -0700

    Complete through layout

--- TutorialApp/PhotoPage.xaml.cs
+++ TutorialApp/PhotoPage.xaml.cs
@@ -19,9 +19,9 @@ namespace TutorialApp
     /// <summary>
     /// A basic page that provides characteristics common to most applications.
     /// </summary>
-    public sealed partial class MainPage : TutorialApp.Common.LayoutAwarePage
+    public sealed partial class PhotoPage : TutorialApp.Common.LayoutAwarePage
     {
-        public MainPage()
+        public PhotoPage()
         {
             this.InitializeComponent();
         }
@@ -37,18 +37,6 @@ namespace TutorialApp
         /// session.  This will be null the first time a page is visited.</param>
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
-            if (pageState != null && pageState.ContainsKey("greetingOutputText"))
-            {
-                greetingOutput.Text = pageState["greetingOutputText"].ToString();
-            }
-
-            Windows.Storage.ApplicationDataContainer roamingSettings =
-                Windows.Storage.ApplicationData.Current.RoamingSettings;
-            if (roamingSettings.Values.ContainsKey("userName"))
-            {
-                nameInput.Text = roamingSettings.Values["userName"].ToString();
-            }
-
         }
 
         /// <summary>
@@ -59,27 +47,6 @@ namespace TutorialApp
         /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
         protected override void SaveState(Dictionary<String, Object> pageState)
         {
-            pageState["greetingOutputText"] = greetingOutput.Text;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            greetingOutput.Text = "Hello, " + nameInput.Text + "!";
-        }
-
-        private void NameInput_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            Windows.Storage.ApplicationDataContainer roamingSettings =
-                Windows.Storage.ApplicationData.Current.RoamingSettings;
-            roamingSettings.Values["userName"] = nameInput.Text;
-        }
-
-        private void PhotoPageButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.Frame != null)
-            {
-                this.Frame.Navigate(typeof(PhotoPage));
-            }
         }
     }
 }
